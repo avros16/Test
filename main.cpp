446213c1f8772ab13f4cdbdd1b3a87ff6f7d0103
avros16 <19015561-avros16@users.noreply.replit.com>
2024-03-03 18:12:40 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+using std::cout;
+  int x=10, y=8;
+
+  if (x > y)
+      cout << "*****\n";
+  else
+      cout << "+++++\n";
+      cout << "-----\n"; 
+  
+
+}
